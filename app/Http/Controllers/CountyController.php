296569bb1d256012dd84a31991b76a52981974d8
afaferz <afa00.papa@gmail.com>
2023-06-19 16:24:40 -0300

    feat: Add application cache

--- app/Http/Controllers/CountyController.php
+++ app/Http/Controllers/CountyController.php
@@ -5,13 +5,14 @@ namespace App\Http\Controllers;
 use App\External\Services\Abstractions\CountyServiceAbstract;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\CountyGetRequest as Request;
+use App\Internal\Cache\CacheHelper;
+use App\Internal\Cache\CountyCache;
 use Illuminate\Http\JsonResponse;
 
 class CountyController extends Controller
 {
     public function __construct(private readonly CountyServiceAbstract $service)
     {
-        // Constructor
     }
 
     /** */
@@ -27,6 +28,12 @@ class CountyController extends Controller
             'page_size' => $pageSize,
 
         ];
+
+        $cacheResult = CountyCache::verifyCache($stateCode, $pageNumber, $pageSize);
+
+        if ($cacheResult) {
+            return $this->sendJson($cacheResult);
+        }
         $response = $this->service->getCounty($requestParams);
 
         return $this->sendJson($response);
