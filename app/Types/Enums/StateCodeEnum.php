56b5fa97e5197de73689fcc2c18a47f97369dbc7
afaferz <afa00.papa@gmail.com>
2023-06-19 11:23:32 -0300

    fix: Change enum types to StateCodes

--- app/Types/Enums/StateCodeEnum.php
+++ app/Types/Enums/StateCodeEnum.php
@@ -30,4 +30,13 @@ enum StateCodeEnum: string
     case SP = 'sp';
     case SE = 'se';
     case TO = 'to';
+    
+    public static function strings(): array
+    {
+        $strings = [];
+        foreach(self::cases() as $case) {
+            $strings[] = $case->value;
+        }
+        return $strings;
+    }
 }
