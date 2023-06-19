bb4b2546d5aaec56bfcffae00ea71810f99a9280
afaferz <afa00.papa@gmail.com>
2023-06-19 11:26:30 -0300

    feat: Add prefix to router api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use App\Http\Middleware\Validation\CountyMiddlewareValidation;
+use App\Http\Controllers\CountyController;
+use App\Types\Enums\StateCodeEnum;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Validation\Rules\Enum;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,4 +20,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('county/{state}/', GetCountyController::class)->middleware(CountyMiddlewareValidation::class)->name('GET_COUNTY');
+Route::prefix('v1')->group(function () {
+    Route::get('/county/{state_code}/', [CountyController::class, 'index'])
+        ->where(['state_code' => '^[A-Za-z]{2}$\z'])
+        ->name('get_county');
+});
